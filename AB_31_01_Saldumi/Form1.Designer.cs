b1005861ff6bb52906bf7bb0176f5ec68890d161
ArtsBro <artsbrokans@gmail.com>
2023-03-07 08:50:39 +0200

    ieraksta failā

--- AB_31_01_Saldumi/Form1.Designer.cs
+++ AB_31_01_Saldumi/Form1.Designer.cs
@@ -39,17 +39,24 @@
             this.chekBox1 = new System.Windows.Forms.CheckBox();
             this.checkbox2 = new System.Windows.Forms.CheckBox();
             this.checkbox3 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.vards = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // but1
             // 
             this.but1.AccessibleRole = System.Windows.Forms.AccessibleRole.Caret;
+            this.but1.BackColor = System.Drawing.Color.DimGray;
+            this.but1.ForeColor = System.Drawing.Color.MediumTurquoise;
             this.but1.Location = new System.Drawing.Point(420, 216);
             this.but1.Name = "but1";
             this.but1.Size = new System.Drawing.Size(140, 61);
             this.but1.TabIndex = 0;
             this.but1.Text = "Darbība";
-            this.but1.UseVisualStyleBackColor = true;
+            this.but1.UseVisualStyleBackColor = false;
             this.but1.Click += new System.EventHandler(this.but1_Click);
             // 
             // nauda
@@ -74,6 +81,7 @@
             // 
             this.label1.AccessibleRole = System.Windows.Forms.AccessibleRole.Caret;
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.WhiteSmoke;
             this.label1.Location = new System.Drawing.Point(278, 155);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(131, 13);
@@ -85,6 +93,7 @@
             // 
             this.label2.AccessibleRole = System.Windows.Forms.AccessibleRole.Caret;
             this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.WhiteSmoke;
             this.label2.Location = new System.Drawing.Point(434, 155);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(126, 13);
@@ -95,7 +104,8 @@
             // 
             this.label3.AccessibleRole = System.Windows.Forms.AccessibleRole.Caret;
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(287, 93);
+            this.label3.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label3.Location = new System.Drawing.Point(276, 317);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(123, 13);
             this.label3.TabIndex = 5;
@@ -106,44 +116,103 @@
             // 
             this.chekBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.Caret;
             this.chekBox1.AutoSize = true;
+            this.chekBox1.BackColor = System.Drawing.Color.WhiteSmoke;
             this.chekBox1.Location = new System.Drawing.Point(279, 260);
             this.chekBox1.Name = "chekBox1";
             this.chekBox1.Size = new System.Drawing.Size(71, 17);
             this.chekBox1.TabIndex = 6;
             this.chekBox1.Text = "Šokolāde";
-            this.chekBox1.UseVisualStyleBackColor = true;
+            this.chekBox1.UseVisualStyleBackColor = false;
             this.chekBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged_1);
             // 
             // checkbox2
             // 
-            this.checkbox2.AccessibleRole = System.Windows.Forms.AccessibleRole.Caret;
+            this.checkbox2.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuPopup;
             this.checkbox2.AutoSize = true;
+            this.checkbox2.BackColor = System.Drawing.Color.WhiteSmoke;
             this.checkbox2.Location = new System.Drawing.Point(279, 237);
             this.checkbox2.Name = "checkbox2";
             this.checkbox2.Size = new System.Drawing.Size(60, 17);
             this.checkbox2.TabIndex = 7;
             this.checkbox2.Text = "cepumi";
-            this.checkbox2.UseVisualStyleBackColor = true;
+            this.checkbox2.UseVisualStyleBackColor = false;
             this.checkbox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
             // 
             // checkbox3
             // 
             this.checkbox3.AccessibleRole = System.Windows.Forms.AccessibleRole.Caret;
             this.checkbox3.AutoSize = true;
+            this.checkbox3.BackColor = System.Drawing.Color.WhiteSmoke;
             this.checkbox3.Location = new System.Drawing.Point(281, 214);
             this.checkbox3.Name = "checkbox3";
             this.checkbox3.Size = new System.Drawing.Size(45, 17);
             this.checkbox3.TabIndex = 8;
             this.checkbox3.Text = "sula";
-            this.checkbox3.UseVisualStyleBackColor = true;
+            this.checkbox3.UseVisualStyleBackColor = false;
             this.checkbox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label4.Location = new System.Drawing.Point(276, 340);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "6.00/kg Sulas cena";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label5.Location = new System.Drawing.Point(276, 366);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "5.00/kg Cepumu cena";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label6.Location = new System.Drawing.Point(274, 397);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(125, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "3.00/kg Šokolādes cena";
+            // 
+            // vards
+            // 
+            this.vards.Location = new System.Drawing.Point(153, 114);
+            this.vards.Name = "vards";
+            this.vards.Size = new System.Drawing.Size(128, 20);
+            this.vards.TabIndex = 12;
+            this.vards.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label7.Location = new System.Drawing.Point(5, 117);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(142, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Ievadiet savu vārdu uzvārdu";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.CornflowerBlue;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.LightCoral;
+            this.BackgroundImage = global::AB_31_01_Saldumi.Properties.Resources.Mani_saldumi_1_koncas_3;
+            this.ClientSize = new System.Drawing.Size(649, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.vards);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.checkbox3);
             this.Controls.Add(this.checkbox2);
             this.Controls.Add(this.chekBox1);
@@ -153,7 +222,7 @@
             this.Controls.Add(this.daudzums);
             this.Controls.Add(this.nauda);
             this.Controls.Add(this.but1);
-            this.ForeColor = System.Drawing.Color.Crimson;
+            this.ForeColor = System.Drawing.Color.Black;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -174,6 +243,11 @@
         private System.Windows.Forms.CheckBox chekBox1;
         private System.Windows.Forms.CheckBox checkbox2;
         private System.Windows.Forms.CheckBox checkbox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox vards;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- AB_31_01_Saldumi/Form1.cs
+++ AB_31_01_Saldumi/Form1.cs
@@ -7,13 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace AB_31_01_Saldumi
 {
     public partial class Form1 : Form
     {
+        
 
-       
         public Form1()
         {
             InitializeComponent();
@@ -36,19 +37,29 @@ namespace AB_31_01_Saldumi
 
         private void tex1_TextChanged(object sender, EventArgs e)
         {
-
+             
         }
 
         private void but1_Click(object sender, EventArgs e)
         {
            
-            {
+            {   
+                string vards = this.vards.Text;
+                string nauda = this.nauda.Text;
                 double nauda_daudzums_eur = Convert.ToDouble(this.nauda.Text);
                 double Rezultats_eur = 0;
                 double cena = 9.88;
                 Rezultats_eur = nauda_daudzums_eur / cena;
                 Rezultats_eur = Math.Round(Rezultats_eur, 2);
                 this.daudzums.Text = Rezultats_eur.ToString();
+                using (StreamWriter writer = new StreamWriter("ceks.txt"))
+                {
+                    //ieraksta failā
+                    writer.WriteLine(vards.ToString());
+                    writer.WriteLine(nauda.ToString());
+                    writer.WriteLine(Rezultats_eur.ToString());
+                }
+
             }
 
         }
@@ -69,6 +80,7 @@ namespace AB_31_01_Saldumi
                 Rezultats_eur = nauda_daudzums_eur / cena;
                 Rezultats_eur = Math.Round(Rezultats_eur, 2);
                 this.daudzums.Text = Rezultats_eur.ToString();
+                
             }
             catch (Exception ex)
             {
@@ -98,6 +110,7 @@ namespace AB_31_01_Saldumi
                 Rezultats_eur = nauda_daudzums_eur / cena;
                 Rezultats_eur = Math.Round(Rezultats_eur, 2);
                 this.daudzums.Text = Rezultats_eur.ToString();
+                
             }
             catch (Exception ex)
             {
@@ -117,6 +130,7 @@ namespace AB_31_01_Saldumi
                 Rezultats_eur = nauda_daudzums_eur / cena;
                 Rezultats_eur = Math.Round(Rezultats_eur, 2);
                 this.daudzums.Text = Rezultats_eur.ToString();
+
             }
             catch (Exception ex)
             {
@@ -126,6 +140,30 @@ namespace AB_31_01_Saldumi
             }
 
 
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            using (StreamWriter writer = new StreamWriter("ceks.txt"))
+            {
+                // Write the result to the file
+                writer.WriteLine(vards.ToString());
+            }
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
 
         }
     }
